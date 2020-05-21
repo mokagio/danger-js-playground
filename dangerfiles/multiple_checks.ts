41417ff09cec8fdb221263741ec1f5271931534f
Gio Lodi <giovanni.lodi42@gmail.com>
2020-05-21 21:21:14 +1000

    Remove .ts extension from imports in multiple_checks.ts
    Danger appends `.ts` to the import when resolving it in a remote
    Dangerfile, which results in a 404ing URL.
    
    I read somewhere that not using the extension is the standard, and I can
    see that the examples in the TypeScript docs,
    https://www.typescriptlang.org/docs/handbook/modules.html, but I
    couldn't find a rationale for it.

--- dangerfiles/multiple_checks.ts
+++ dangerfiles/multiple_checks.ts
@@ -1,7 +1,7 @@
 export default async () => {
   // The imports need to be done within this async function.
-  const {checkLabel} = await import("./label.ts")
-  const {checkMilestone} = await import("./milestone.ts")
+  const {checkLabel} = await import("./label")
+  const {checkMilestone} = await import("./milestone")
 
   await checkLabel()
   await checkMilestone()
