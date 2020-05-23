7df86f8ff85e613d16bad4a7a0f76872309fa27e
Gio Lodi <giovanni.lodi42@gmail.com>
2020-05-23 21:50:05 +1000

    Test build and pass with this setup - danger ci fails

--- dangerfiles/label.ts
+++ dangerfiles/label.ts
@@ -1,7 +1,7 @@
 // The import is not required for the file to run standalone and it actually
 // results in a runtime error when importing this file.
 //
-// import {warn, danger} from "danger";
+import {warn, danger} from "danger";
 
 export async function checkLabel() {
   const labels = danger.github.issue.labels;
